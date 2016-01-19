797b122d794de3984247f24f43a087bf4b7fa685
clewinth <clewinthal@urban.org>
2016-01-18 19:42:42 -0500

    Upload Monday 18 Jan
    Restyled again, fixed form alignment, updated sections

--- js/main.js
+++ js/main.js
@@ -6,3 +6,53 @@ document.getElementsByTagName("li")[0].addEventListener("click",function(){
 		this.parentNode.removeAttribute("class");
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+Schedule form
+name - first, last
+effective date - today
+<script>
+   document.getElementById('date').value = (new Date()).format("m/dd/yy");
+</script>
+new schedule - 100%, 80%, 75%, 60%, 50%
+confirm
+
+$( "form" ).change(function() {
+	console.log('form')
+	event.preventDefault()
+
+	var new-schedule = $()
+}
+
+
+Address form
+name - first, last
+effective date - today
+address 1
+address 2
+city
+state
+zip
+confirm
+
+
+Commuting form
+name - first, last
+effective month - jan thru dec
+deduction - transit
+deduction - parking
+total
+confirm
+
+
+
+
